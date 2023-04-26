832f908e3abcc69490d33670fa2cbfcf13bd14db
Christie Griffiths <christiegriffiths@outlook.com>
2023-04-26 10:36:20 +0100

    week 3 Wednesday morning update

---
+++ phase2/lib/albums.rb
@@ -0,0 +1,4 @@
+class Albums
+  # Replace the attributes by your own columns.
+  attr_accessor :id, :title, :release_year, :artist_id
+end
---
+++ phase2/spec/albums_repository_spec.rb
@@ -0,0 +1,25 @@
+require'albums_repository'
+
+describe AlbumsRepository do 
+  def reset_albums_table
+    seed_sql = File.read('spec/seeds_music_library.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) do 
+    reset_albums_table
+  end 
+
+  it '' do
+    repo = AlbumsRepository.new
+    albums = repo.all 
+
+    expect(albums.length).to eq 2
+    expect(albums.first.id).to eq "1"
+    expect(albums.first.title).to eq "The River"
+    expect(albums.first.artist_id).to eq nil
+  end
+end 
+
+
